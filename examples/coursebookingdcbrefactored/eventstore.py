be033b3cd1f31dc3a6209d4ea5f6586d618e7869
johnbywater <john.bywater@appropriatesoftware.net>
2025-05-28 16:16:35 +0100

    Moved events up and event store down.

--- examples/coursebookingdcbrefactored/eventstore.py
+++ examples/coursebookingdcbrefactored/eventstore.py
@@ -22,6 +22,226 @@ if TYPE_CHECKING:
     from collections.abc import Sequence
 
 
+
+_enduring_object_init_classes: dict[type[Any], type[Initialised]] = {}
+
+
+class CanMutateEnduringObject(AbstractDCBEvent):
+    tags: list[str]
+
+    def _as_dict(self) -> dict[str, Any]:
+        raise NotImplementedError
+
+    def mutate(self, obj: EnduringObject | None) -> EnduringObject | None:
+        assert obj is not None
+        self.apply(obj)
+        return obj
+
+    def apply(self, obj: Any) -> None:
+        pass
+
+
+class CanInitialiseEnduringObject(CanMutateEnduringObject):
+    originator_topic: str
+
+    def mutate(self, obj: EnduringObject | None) -> EnduringObject | None:
+        kwargs = self._as_dict()
+        originator_topic = kwargs.pop("originator_topic")
+        enduring_object_cls = cast(
+            type[EnduringObject], resolve_topic(originator_topic)
+        )
+        enduring_object = enduring_object_cls.__new__(enduring_object_cls)
+        kwargs.pop("tags")
+        common_kwargs = {
+            "id": kwargs.pop(enduring_object_cls.id_attr_name),
+        }
+        enduring_object.__base_init__(**common_kwargs)
+        enduring_object.__post_init__()
+        try:
+            enduring_object.__init__(**kwargs)  # type: ignore[misc]
+        except TypeError as e:
+            msg = (f"{type(self).__qualname__} can't __init__ "
+                   f"{enduring_object_cls.__qualname__} "
+                   f"with kwargs {kwargs}: {e}")
+            raise TypeError(msg) from e
+        return enduring_object
+
+
+class MetaPerspective(type):
+    def __init__(self, name: str, bases: tuple[type, ...], namespace: dict[str, Any]) -> None:
+        super().__init__(name, bases, namespace)
+        for attr, value in namespace.items():
+            if isinstance(value, CommandMethodDecorator):
+                # Just keep things simple.
+                # TODO: Maybe support event name strings, maybe not....
+                assert value.given_event_cls is not None, "Event class not given"
+                # TODO: Actually maybe enforce that given event class is nested on self.
+                event_cls_qualname = f"{self.__qualname__}.{value.given_event_cls.__name__}"
+                event_cls_dict = {
+                    # "__annotations__": annotations,
+                    "__module__": self.__module__,
+                    "__qualname__": event_cls_qualname,
+                }
+
+                event_subclass = type(name, (DecoratorEvent, value.given_event_cls), event_cls_dict)
+                namespace[attr] = event_subclass
+                decorator_event_classes[value] = event_subclass
+                decorated_funcs[event_subclass] = value.decorated_func
+
+
+class Perspective(metaclass=MetaPerspective):
+    def __init__(self, *objs: EnduringObject | None) -> None:  # noqa: A002
+        self.new_decisions: list[Decision] = []
+        self.last_known_position: int | None = None
+
+    def collect_events(self) -> list[Decision]:
+        collected, self.new_decisions = self.new_decisions, []
+        return collected
+
+    @property
+    def cb(self) -> list[Selector]:
+        raise NotImplementedError
+
+T = TypeVar("T")
+
+
+class MetaEnduringObject(MetaPerspective):
+    def __init__(cls, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        for item in cls.__dict__.values():
+            if isinstance(item, type) and issubclass(item, Initialised):
+                _enduring_object_init_classes[cls] = item
+                break
+
+    def __call__(cls: type[T], **kwargs: Any) -> T:
+        # TODO: For convenience, make this error out in the same way
+        #  as it would if the arguments didn't match the __init__
+        #  method and __init__was called directly, and verify the
+        #  event's __init__ is valid when initialising the class.
+
+        assert issubclass(cls, EnduringObject)
+        try:
+            init_enduring_object_class = _enduring_object_init_classes[cls]
+        except KeyError:
+            msg = (
+                f"Enduring object class {cls.__name__} has no CanInitEnduringObject "
+                f"class. Please define a subclass of CanInitEnduringObject"
+                f"as a nested class on {cls.__name__}."
+            )
+            raise ProgrammingError(msg) from None
+
+        return cast(
+            T,
+            cls._create(
+                decision_cls=init_enduring_object_class,
+                **kwargs,
+            ),
+        )
+
+    @property
+    def id_attr_name(cls) -> str:
+        return f"{cls.__name__.lower()}_id"
+
+
+class DecoratorEvent(CanMutateEnduringObject):
+    def apply(self, obj: Perspective) -> None:
+        """Applies event to perspective by calling method decorated by @event."""
+        # Identify the function that was decorated.
+        decorated_func = decorated_funcs[type(self)]
+
+        # Select event attributes mentioned in function signature.
+        self_dict = self._as_dict()
+        kwargs = filter_kwargs_for_method_params(self_dict, decorated_func)
+
+        # Call the original method with event attribute values.
+        decorated_method = decorated_func.__get__(obj, type(obj))
+        decorated_method(**kwargs)
+
+        # Call super method, just in case any base classes need it.
+        super().apply(obj)
+
+
+# Introduce msgspec.Struct for transcoding.
+class Decision(Struct, CanMutateEnduringObject):
+    tags: list[str]
+
+    def _as_dict(self) -> dict[str, Any]:
+        return {key: getattr(self, key) for key in self.__struct_fields__}
+
+
+class Initialised(Decision, CanInitialiseEnduringObject):
+    originator_topic: str
+
+
+class EnduringObject(Perspective, metaclass=MetaEnduringObject):
+    @classmethod
+    def _create(cls: type[Self], decision_cls: type[Initialised], **kwargs: Any) -> Self:
+        enduring_object_id = cls._create_id()
+        initial_kwargs: dict[str, Any] = {cls.id_attr_name: enduring_object_id}
+        initial_kwargs.update(kwargs)
+        initial_kwargs["originator_topic"] = get_topic(cls)
+        initial_kwargs["tags"] = [enduring_object_id]
+        try:
+            initialised = decision_cls(**initial_kwargs)
+        except TypeError as e:
+            msg = (
+                f"Unable to construct {decision_cls.__qualname__} event "
+                f"with kwargs {initial_kwargs}: {e}"
+            )
+            raise TypeError(msg) from e
+        enduring_object = cast(Self, initialised.mutate(None))
+        assert enduring_object is not None
+        enduring_object.new_decisions.append(initialised)
+        return enduring_object
+
+    @classmethod
+    def _create_id(cls) -> str:
+        return f"{cls.__name__.lower()}-{uuid4()}"
+
+    def __base_init__(self, id: str) -> None:  # noqa: A002
+        super().__init__()
+        self.id = id
+
+    def __post_init__(self) -> None:
+        pass
+
+    @property
+    def cb(self) -> list[Selector]:
+        return [Selector(tags=[self.id])]
+
+    def trigger_event(
+        self, decision_cls: type[Decision], *, tags: Sequence[str] = (), **kwargs: Any
+    ) -> None:
+        tags = [self.id, *tags]
+        decision = decision_cls(tags=tags, **kwargs)
+        decision.mutate(self)
+        self.new_decisions.append(decision)
+
+
+class Group(Perspective):
+    @property
+    def cb(self) -> list[Selector]:
+        return self._flatten(
+            [o.cb for o in self.__dict__.values() if isinstance(o, EnduringObject)]
+        )
+
+    def _flatten(self, xss: list[list[Selector]]) -> list[Selector]:
+        return [x for xs in xss for x in xs]
+
+    def trigger_event(
+        self, decision_cls: type[Decision], *, tags: Sequence[str] = (), **kwargs: Any
+    ) -> None:
+        objs = [o for o in self.__dict__.values() if isinstance(o, EnduringObject)]
+        tags = [o.id for o in objs] + list(tags)
+        decision = decision_cls(tags=tags, **kwargs)
+        for o in objs:
+            decision.mutate(o)
+        self.new_decisions.append(decision)
+
+
+TGroup = TypeVar("TGroup", bound=Group)
+
+
 class Mapper:
     def to_dcb_event(self, event: Decision) -> DCBEvent:
         return DCBEvent(
@@ -207,221 +427,3 @@ class Repository:
 class NotFoundError(Exception):
     pass
 
-
-_enduring_object_init_classes: dict[type[Any], type[Initialised]] = {}
-
-
-class CanMutateEnduringObject(AbstractDCBEvent):
-    tags: list[str]
-
-    def _as_dict(self) -> dict[str, Any]:
-        raise NotImplementedError
-
-    def mutate(self, obj: EnduringObject | None) -> EnduringObject | None:
-        assert obj is not None
-        self.apply(obj)
-        return obj
-
-    def apply(self, obj: Any) -> None:
-        pass
-
-
-class CanInitialiseEnduringObject(CanMutateEnduringObject):
-    originator_topic: str
-
-    def mutate(self, obj: EnduringObject | None) -> EnduringObject | None:
-        kwargs = self._as_dict()
-        originator_topic = kwargs.pop("originator_topic")
-        enduring_object_cls = cast(
-            type[EnduringObject], resolve_topic(originator_topic)
-        )
-        enduring_object = enduring_object_cls.__new__(enduring_object_cls)
-        kwargs.pop("tags")
-        common_kwargs = {
-            "id": kwargs.pop(enduring_object_cls.id_attr_name),
-        }
-        enduring_object.__base_init__(**common_kwargs)
-        enduring_object.__post_init__()
-        try:
-            enduring_object.__init__(**kwargs)  # type: ignore[misc]
-        except TypeError as e:
-            msg = (f"{type(self).__qualname__} can't __init__ "
-                   f"{enduring_object_cls.__qualname__} "
-                   f"with kwargs {kwargs}: {e}")
-            raise TypeError(msg) from e
-        return enduring_object
-
-
-class MetaPerspective(type):
-    def __init__(self, name: str, bases: tuple[type, ...], namespace: dict[str, Any]) -> None:
-        super().__init__(name, bases, namespace)
-        for attr, value in namespace.items():
-            if isinstance(value, CommandMethodDecorator):
-                # Just keep things simple.
-                # TODO: Maybe support event name strings, maybe not....
-                assert value.given_event_cls is not None, "Event class not given"
-                # TODO: Actually maybe enforce that given event class is nested on self.
-                event_cls_qualname = f"{self.__qualname__}.{value.given_event_cls.__name__}"
-                event_cls_dict = {
-                    # "__annotations__": annotations,
-                    "__module__": self.__module__,
-                    "__qualname__": event_cls_qualname,
-                }
-
-                event_subclass = type(name, (DecoratorEvent, value.given_event_cls), event_cls_dict)
-                namespace[attr] = event_subclass
-                decorator_event_classes[value] = event_subclass
-                decorated_funcs[event_subclass] = value.decorated_func
-
-
-class Perspective(metaclass=MetaPerspective):
-    def __init__(self, *objs: EnduringObject | None) -> None:  # noqa: A002
-        self.new_decisions: list[Decision] = []
-        self.last_known_position: int | None = None
-
-    def collect_events(self) -> list[Decision]:
-        collected, self.new_decisions = self.new_decisions, []
-        return collected
-
-    @property
-    def cb(self) -> list[Selector]:
-        raise NotImplementedError
-
-T = TypeVar("T")
-
-
-class MetaEnduringObject(MetaPerspective):
-    def __init__(cls, *args: Any, **kwargs: Any) -> None:
-        super().__init__(*args, **kwargs)
-        for item in cls.__dict__.values():
-            if isinstance(item, type) and issubclass(item, Initialised):
-                _enduring_object_init_classes[cls] = item
-                break
-
-    def __call__(cls: type[T], **kwargs: Any) -> T:
-        # TODO: For convenience, make this error out in the same way
-        #  as it would if the arguments didn't match the __init__
-        #  method and __init__was called directly, and verify the
-        #  event's __init__ is valid when initialising the class.
-
-        assert issubclass(cls, EnduringObject)
-        try:
-            init_enduring_object_class = _enduring_object_init_classes[cls]
-        except KeyError:
-            msg = (
-                f"Enduring object class {cls.__name__} has no CanInitEnduringObject "
-                f"class. Please define a subclass of CanInitEnduringObject"
-                f"as a nested class on {cls.__name__}."
-            )
-            raise ProgrammingError(msg) from None
-
-        return cast(
-            T,
-            cls._create(
-                decision_cls=init_enduring_object_class,
-                **kwargs,
-            ),
-        )
-
-    @property
-    def id_attr_name(cls) -> str:
-        return f"{cls.__name__.lower()}_id"
-
-
-class Decision(Struct, CanMutateEnduringObject):
-    tags: list[str]
-
-    def _as_dict(self) -> dict[str, Any]:
-        return {key: getattr(self, key) for key in self.__struct_fields__}
-
-
-class Initialised(Decision, CanInitialiseEnduringObject):
-    originator_topic: str
-
-
-class DecoratorEvent(Decision):
-    def apply(self, obj: Perspective) -> None:
-        """Applies event to perspective by calling method decorated by @event."""
-        # Identify the function that was decorated.
-        decorated_func = decorated_funcs[type(self)]
-
-        # Select event attributes mentioned in function signature.
-        self_dict = self._as_dict()
-        kwargs = filter_kwargs_for_method_params(self_dict, decorated_func)
-
-        # Call the original method with event attribute values.
-        decorated_method = decorated_func.__get__(obj, type(obj))
-        decorated_method(**kwargs)
-
-        # Call super method, just in case any base classes need it.
-        super().apply(obj)
-
-
-class EnduringObject(Perspective, metaclass=MetaEnduringObject):
-    @classmethod
-    def _create(cls: type[Self], decision_cls: type[Initialised], **kwargs: Any) -> Self:
-        enduring_object_id = cls._create_id()
-        initial_kwargs: dict[str, Any] = {cls.id_attr_name: enduring_object_id}
-        initial_kwargs.update(kwargs)
-        initial_kwargs["originator_topic"] = get_topic(cls)
-        initial_kwargs["tags"] = [enduring_object_id]
-        try:
-            initialised = decision_cls(**initial_kwargs)
-        except TypeError as e:
-            msg = (
-                f"Unable to construct {decision_cls.__qualname__} event "
-                f"with kwargs {initial_kwargs}: {e}"
-            )
-            raise TypeError(msg) from e
-        enduring_object = cast(Self, initialised.mutate(None))
-        assert enduring_object is not None
-        enduring_object.new_decisions.append(initialised)
-        return enduring_object
-
-    @classmethod
-    def _create_id(cls) -> str:
-        return f"{cls.__name__.lower()}-{uuid4()}"
-
-    def __base_init__(self, id: str) -> None:  # noqa: A002
-        super().__init__()
-        self.id = id
-
-    def __post_init__(self) -> None:
-        pass
-
-    @property
-    def cb(self) -> list[Selector]:
-        return [Selector(tags=[self.id])]
-
-    def trigger_event(
-        self, decision_cls: type[Decision], *, tags: Sequence[str] = (), **kwargs: Any
-    ) -> None:
-        tags = [self.id, *tags]
-        decision = decision_cls(tags=tags, **kwargs)
-        decision.mutate(self)
-        self.new_decisions.append(decision)
-
-
-class Group(Perspective):
-    @property
-    def cb(self) -> list[Selector]:
-        return self._flatten(
-            [o.cb for o in self.__dict__.values() if isinstance(o, EnduringObject)]
-        )
-
-    def _flatten(self, xss: list[list[Selector]]) -> list[Selector]:
-        return [x for xs in xss for x in xs]
-
-    def trigger_event(
-        self, decision_cls: type[Decision], *, tags: Sequence[str] = (), **kwargs: Any
-    ) -> None:
-        objs = [o for o in self.__dict__.values() if isinstance(o, EnduringObject)]
-        tags = [o.id for o in objs] + list(tags)
-        decision = decision_cls(tags=tags, **kwargs)
-        for o in objs:
-            decision.mutate(o)
-        self.new_decisions.append(decision)
-
-
-
-TGroup = TypeVar("TGroup", bound=Group)
