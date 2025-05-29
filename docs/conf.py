c4e638ca0a370510db5f852451cee76c146ff835
johnbywater <john.bywater@appropriatesoftware.net>
2025-05-29 21:19:31 +0100

    Fixed docs config for μ.

--- docs/conf.py
+++ docs/conf.py
@@ -99,3 +99,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# For the μ character in the "speedrun" reports.
+# https://docs.readthedocs.com/platform/latest/guides/pdf-non-ascii-languages.html
+latex_engine = "xelatex"
